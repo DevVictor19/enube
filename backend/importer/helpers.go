891ef9666b29f7a81cf59adf4d6cf374c58d6510
DevVictor19 <antoniovictor12@live.com>
2025-07-10 16:30:27 -0300

    feat: add build batch insert helper

---
+++ backend/importer/helpers.go
@@ -0,0 +1,37 @@
+package importer
+
+import (
+	"fmt"
+	"strings"
+)
+
+func buildBatchInsert(tableName string, columns []string, totalValues int) string {
+	if totalValues == 0 || len(columns) == 0 || totalValues%len(columns) != 0 {
+		return ""
+	}
+
+	rows := totalValues / len(columns)
+	var builder strings.Builder
+
+	builder.WriteString("INSERT INTO ")
+	builder.WriteString(tableName)
+	builder.WriteString(" (" + strings.Join(columns, ", ") + ") VALUES ")
+
+	for i := 0; i < rows; i++ {
+		builder.WriteString("(")
+		for j := 0; j < len(columns); j++ {
+			paramIndex := i*len(columns) + j + 1
+			builder.WriteString(fmt.Sprintf("$%d", paramIndex))
+			if j < len(columns)-1 {
+				builder.WriteString(", ")
+			}
+		}
+		builder.WriteString(")")
+		if i < rows-1 {
+			builder.WriteString(", ")
+		}
+	}
+
+	builder.WriteString(";")
+	return builder.String()
+}
