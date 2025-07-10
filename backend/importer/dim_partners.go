133c10d15f11df688b182f94ff9cc3a8c7cd374f
DevVictor19 <antoniovictor12@live.com>
2025-07-10 16:31:23 -0300

    feat: add partner module

---
+++ backend/importer/dim_partners.go
@@ -0,0 +1,44 @@
+package importer
+
+var (
+	partnerSequence = 0
+	partners        map[string]int // partnerId | sk
+	partnerValues   []any
+)
+
+func getPartnerSk(row []string) int {
+	if partners == nil {
+		partners = make(map[string]int)
+	}
+
+	partnerId := row[partnerIdIndex]
+	partnerName := row[partnerNameIndex]
+	mpnId := row[mpnIdIndex]
+	invoiceNumber := row[invoiceNumberIndex]
+
+	existentSequence, ok := partners[partnerId]
+	if !ok {
+		partnerSequence++
+		partners[partnerId] = partnerSequence
+
+		partnerValues = append(
+			partnerValues,
+			partnerSequence,
+			partnerId,
+			partnerName,
+			mpnId,
+			invoiceNumber,
+		)
+
+		return partnerSequence
+	}
+
+	return existentSequence
+}
+
+func getPartnerStm() string {
+	table := "dim_partners"
+	cols := []string{"partner_sk", "partner_id", "partner_name", "mpn_id", "invoice_number"}
+	totalVals := len(partnerValues)
+	return buildBatchInsert(table, cols, totalVals)
+}
