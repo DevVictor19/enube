e54c01c1b9d287ba0379357c22215ea22edbad67
DevVictor19 <antoniovictor12@live.com>
2025-07-10 17:39:49 -0300

    feat: add last dimensions

---
+++ backend/importer/dim_meters.go
@@ -0,0 +1,59 @@
+package importer
+
+var (
+	meterSequence = 0
+	meters        map[string]int
+	meterValues   []any
+)
+
+func getMeterSk(row []string) int {
+	if meters == nil {
+		meters = make(map[string]int)
+	}
+
+	meterId := row[meterIdIndex]
+	name := row[meterNameIndex]
+	category := row[meterCategoryIndex]
+	meterType := row[meterTypeIndex]
+	subcategory := row[meterSubCategoryIndex]
+	region := row[meterRegionIndex]
+	unit := row[meterUnitIndex]
+
+	existentSequence, ok := meters[meterId]
+	if !ok {
+		meterSequence++
+		meters[meterId] = meterSequence
+
+		meterValues = append(
+			meterValues,
+			meterSequence,
+			meterId,
+			name,
+			category,
+			meterType,
+			subcategory,
+			region,
+			unit,
+		)
+
+		return meterSequence
+	}
+
+	return existentSequence
+}
+
+func getMeterStm() string {
+	table := "dim_meters"
+	cols := []string{
+		"meter_sk",
+		"meter_id",
+		"name",
+		"category",
+		"type",
+		"subcategory",
+		"region",
+		"unit",
+	}
+	totalVals := len(meterValues)
+	return buildBatchInsert(table, cols, totalVals)
+}
---
+++ backend/importer/dim_months_charge_dates.go
@@ -0,0 +1,48 @@
+package importer
+
+import "fmt"
+
+var (
+	monthsChargeDateSequence = 0
+	monthsChargeDates        map[string]int
+	monthsChargeDateValues   []any
+)
+
+func getMonthsChargeDateSk(row []string) int {
+	if monthsChargeDates == nil {
+		monthsChargeDates = make(map[string]int)
+	}
+
+	chargeStartDate := row[chargeStartDateIndex]
+	chargeEndDate := row[chargeEndDateIndex]
+
+	key := fmt.Sprintf("%s|%s", chargeStartDate, chargeEndDate)
+
+	existentSequence, ok := monthsChargeDates[key]
+	if !ok {
+		monthsChargeDateSequence++
+		monthsChargeDates[key] = monthsChargeDateSequence
+
+		monthsChargeDateValues = append(
+			monthsChargeDateValues,
+			monthsChargeDateSequence,
+			chargeStartDate,
+			chargeEndDate,
+		)
+
+		return monthsChargeDateSequence
+	}
+
+	return existentSequence
+}
+
+func getMonthsChargeDateStm() string {
+	table := "dim_months_charge_dates"
+	cols := []string{
+		"months_charge_date_sk",
+		"charge_start_date",
+		"charge_end_date",
+	}
+	totalVals := len(monthsChargeDateValues)
+	return buildBatchInsert(table, cols, totalVals)
+}
