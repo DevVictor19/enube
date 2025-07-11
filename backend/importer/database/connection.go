6d140f2079da8a337e75425b5baec040fc6e0f1d
DevVictor19 <antoniovictor12@live.com>
2025-07-11 13:55:45 -0300

    feat: add db configs

--- backend/importer/database/connection.go
+++ backend/importer/database/connection.go
@@ -4,12 +4,22 @@ import (
 	"context"
 	"database/sql"
 	"errors"
+	"fmt"
+	"os"
+	"path/filepath"
+	"runtime"
 	"time"
 
+	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
 )
 
-const QueryDuration = time.Second * 5
+const (
+	QueryDuration = time.Second * 5
+	maxOpenConns  = 30
+	maxIdleConns  = 30
+	maxIdleTime   = "15m"
+)
 
 var dbRef *sql.DB
 
@@ -20,7 +30,25 @@ func Get() (*sql.DB, error) {
 	return dbRef, nil
 }
 
-func Connect(url string, maxOpenConns, maxIdleConns int, maxIdleTime string) (*sql.DB, error) {
+func Connect() (*sql.DB, error) {
+	_, filename, _, ok := runtime.Caller(0)
+	if !ok {
+		return nil, fmt.Errorf("could not get current file path")
+	}
+
+	currentDir := filepath.Dir(filename)
+	projectRootPath := filepath.Join(currentDir, "..", "..")
+	envPath := filepath.Join(projectRootPath, ".env")
+
+	if err := godotenv.Load(envPath); err != nil {
+		return nil, err
+	}
+
+	url := os.Getenv("DB_URL")
+	if url == "" {
+		return nil, fmt.Errorf("DB_URL env variable is empty")
+	}
+
 	db, err := sql.Open("postgres", url)
 	if err != nil {
 		return nil, err
