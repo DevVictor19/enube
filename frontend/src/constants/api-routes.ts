5cf27ed26a32c73f425fdaadf371c845ea08bb49
DevVictor19 <antoniovictor12@live.com>
2025-07-13 09:42:12 -0300

    feat: add api routes constants

---
+++ frontend/src/constants/api-routes.ts
@@ -0,0 +1,14 @@
+export const API_ROUTES = {
+  LOGIN: "auth/login",
+  CHARGES: "/charges",
+  CHARGES_RESUME: "/charges/resume",
+  CUSTOMERS: "/customers",
+  PRODUCTS: "/products",
+  PARTNERS: "/partners",
+  MONTHS_CHARGE_DATES: "/months_charge_dates",
+  USAGE_DATES: "/usage_dates",
+  BILLING_CURRENCIES: "/billing_currencies",
+  PRICING_CURRENCIES: "/pricing_currencies",
+  RESOURCE_LOCATIONS: "/resource_locations",
+  SERVICES: "/services",
+} as const;
