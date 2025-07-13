481eb5acad92e9b080321179d2f8ba910fe3f38d
DevVictor19 <antoniovictor12@live.com>
2025-07-13 08:29:10 -0300

    feat: add css baseline

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,9 +5,11 @@ import "@fontsource/roboto/700.css";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
+import CssBaseline from "@mui/material/CssBaseline";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
+    <CssBaseline />
     <App />
   </StrictMode>
 );
