52aad76120031bdd2164d2dc0b8bda768526656d
DevVictor19 <antoniovictor12@live.com>
2025-08-30 16:04:22 -0300

    refactor: remove tables const

--- backend/importerV2/consts.go
+++ backend/importerV2/consts.go
@@ -24,33 +24,6 @@ var (
 	usageDatesCache        tableCache
 )
 
-type tableName string
-
-const (
-	availabilitiesTable    tableName = "dim_availabilities"
-	benefitOrdersTable     tableName = "dim_benefit_orders"
-	benefitsTable          tableName = "dim_benefits"
-	billingCurrenciesTable tableName = "dim_billing_currencies"
-	chargeTypesTable       tableName = "dim_charge_types"
-	customersTable         tableName = "dim_customers"
-	entitlementsTable      tableName = "dim_entitlements"
-	metersTable            tableName = "dim_meters"
-	monthsChargeDatesTable tableName = "dim_months_charge_dates"
-	partnerCreditsTable    tableName = "dim_partner_credits"
-	partnersTable          tableName = "dim_partners"
-	pricingCurrenciesTable tableName = "dim_pricing_currencies"
-	productsTable          tableName = "dim_products"
-	publishersTable        tableName = "dim_publishers"
-	resourceGroupsTable    tableName = "dim_resource_groups"
-	resourceLocationsTable tableName = "dim_resource_locations"
-	servicesTable          tableName = "dim_services"
-	skusTable              tableName = "dim_skus"
-	subscriptionsTable     tableName = "dim_subscriptions"
-	unitTypesTable         tableName = "dim_unit_types"
-	usageDatesTable        tableName = "dim_usage_dates"
-	factChargesTable       tableName = "fact_charges"
-)
-
 const (
 	partnerIdIndex int = iota
 	partnerNameIndex
